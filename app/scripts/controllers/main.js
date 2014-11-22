95c80197c3f52374ea55ffb279e408b6cffaf210
pranavpr <pranav@pranavprakash.net>
2014-11-22 18:22:40 +0530

    Managing Lists

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,43 +10,24 @@
 angular.module('angularApp')
   .controller('MainCtrl', function ($scope, $timeout) {
   	var rootRef = new Firebase('https://popping-fire-8603.firebaseio.com/');
-  	var childRef = rootRef.child('message');
+  	var messagesRef = rootRef.child('messages');
 
-  	childRef.on('value', function(snapshot) {
-  		snapshot.forEach(function(item){
-  			console.log(item.key() + ' - ' + item.val());
-  		});
+  	$scope.currentUser = null;
+  	$scope.currentText = null;
+
+  	messagesRef.on('value', function(snapshot) {
   		$timeout(function() {
-  			console.log(snapshot.val());
-  			$scope.message = snapshot.val();
+  			var snapshotVal = snapshot.val();
+  			$scope.messages = snapshotVal;
   		});
   	});
 
-  	$scope.$watch('message.text', function(newVal){
-  		if(!newVal) {
-  			return;
-  		}
-  		childRef.update({
-  			text: newVal
-  		});
-  	});
-  	$scope.setMessage = function()
-  	{
-  		childRef.set({
-  			user: 'Bob',
-  			text: 'Hi'
-  		});
+  	$scope.sendMessage = function() {
+  		var newMessage = {
+  			user: $scope.currentUser,
+  			text: $scope.currentText
+  		};
+  		messagesRef.push(newMessage);
   	};
 
-  	$scope.updateMessage = function()
-  	{
-  		childRef.update({
-  			text: 'Bye'
-  		});
-  	};
-
-  	$scope.deleteMessage = function()
-  	{
-  		childRef.remove();
-  	};
   });
