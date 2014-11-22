bd71202ca8602bb24174d194ee516bd945f61d50
pranavpr <pranav.iitkgp@gmail.com>
2014-11-22 21:26:56 +0530

    On child added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,26 +8,29 @@
  * Controller of the angularApp
  */
 angular.module('angularApp')
-  .controller('MainCtrl', function ($scope, $timeout) {
-  	var rootRef = new Firebase('https://popping-fire-8603.firebaseio.com/');
-  	var messagesRef = rootRef.child('messages');
+  .controller('MainCtrl', function($scope, $timeout) {
+    var rootRef = new Firebase('https://popping-fire-8603.firebaseio.com/');
+    var messagesRef = rootRef.child('messages');
 
-  	$scope.currentUser = null;
-  	$scope.currentText = null;
+    $scope.currentUser = null;
+    $scope.currentText = null;
+    $scope.messages = [];
 
-  	messagesRef.on('value', function(snapshot) {
-  		$timeout(function() {
-  			var snapshotVal = snapshot.val();
-  			$scope.messages = snapshotVal;
-  		});
-  	});
+    messagesRef.on('child_added', function(snapshot) {
+      $timeout(function() {
+        var snapshotVal = snapshot.val();
+        $scope.messages.push(snapshotVal);
+      });
+    });
 
-  	$scope.sendMessage = function() {
-  		var newMessage = {
-  			user: $scope.currentUser,
-  			text: $scope.currentText
-  		};
-  		messagesRef.push(newMessage);
-  	};
+    $scope.sendMessage = function() {
+      var newMessage = {
+        user: $scope.currentUser,
+        text: $scope.currentText
+      };
+      messagesRef.push(newMessage);
+      $scope.currentUser = null;
+      $scope.currentText = null;
+    };
 
   });
